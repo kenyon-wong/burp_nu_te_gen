add7149174826475c8b513c3be626ea9d3ff1276
kenyon <kenyon.wong@icloud.com>
2024-02-02 14:36:40 +0800

    update
    Signed-off-by: kenyon <kenyon.wong@icloud.com>

--- src/main/java/burp/HelpDataLoader.java
+++ src/main/java/burp/HelpDataLoader.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 
 public class HelpDataLoader {
     public static String loadHelpData(String resourcePath) {
-        try (InputStream inputStream = HelpDataLoader.class.getResourceAsStream(resourcePath);
-             Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
-            return scanner.useDelimiter("\\A").next();
+        try (InputStream inputStream = HelpDataLoader.class.getResourceAsStream(resourcePath)) {
+	        assert inputStream != null;
+	        try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
+	            return scanner.useDelimiter("\\A").next();
+	        }
         } catch (Exception e) {
             throw new RuntimeException("Failed to load help data from resource: " + resourcePath, e);
         }
